646f75a520bd4f6573fa9346f7ba3f3542666e11
iyeaaa <iyeaaa@naver.com>
2022-03-30 21:54:31 +0900

    Create 1931

---
+++ BOJ_Swift/main.swift
@@ -0,0 +1,13 @@
+infix operator **
+
+func ** (value1: Int, value2: Int) -> Int {
+    var temp: Int = value1
+
+    for _ in 1..<value2 {
+        temp *= value1
+    }
+
+    return temp
+}
+
+print(5**3)

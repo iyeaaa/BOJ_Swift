47d59d01ddaebc4be04778cb884c291594fe331f
iyeaaa <iyeaaa@naver.com>
2022-03-31 15:02:57 +0900

    Create 1931

--- BOJ_Swift/1931.swift
+++ BOJ_Swift/1931.swift
@@ -1,18 +1,27 @@
-var n = Int(readLine()!)!
-var result = ""
+let n = Int(readLine()!)!
+var meet: [[Int]] = Array(repeating: Array(repeating: 0, count: 2), count: n)
 
-for _ in 0..<n {
-    let l: Array<String> = readLine()!.split{$0 == " "}.map{String($0)}
-    let rec: Int = Int(l[0])!
-    let str: String = l[1]
+for i in 0...n-1 {
+    let met = readLine()!.split(separator: " ").map{Int(String($0))!}
+    meet[i][0] = met[0]
+    meet[i][1] = met[1]
+}
 
-    for ch in str {
-        for _ in 0..<rec {
-            result += String(ch)
-        }
+meet.sort(by: {
+    if $0[1] == $1[1] {
+        return $0[0] < $1[0]
     }
+    return $0[1] < $1[1]
+})
 
-    result += "\n"
+var minStart = meet[0][1]
+var result = 1
+
+for i in 1...n-1 {
+    if meet[i][0] >= minStart {
+        result += 1
+        minStart = meet[i][1]
+    }
 }
 
 print(result)

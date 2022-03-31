9e415e761426972073432a4199acd2b3c6105214
iyeaaa <iyeaaa@naver.com>
2022-03-31 20:20:11 +0900

    Create 11399

--- BOJ_Swift/11399.swift
+++ BOJ_Swift/11399.swift
@@ -1,11 +1,12 @@
 let num = Int(readLine()!)!
 let line = readLine()!.split(separator: " ").map{Int(String($0))!}.sorted(by: <)
+
 var answer = 0
+var prev_sum = 0
 
 for i in 0..<num {
-    for j in 0...i {
-        answer += line[j]
-    }
+    prev_sum += line[i]
+    answer += prev_sum
 }
 
 print(answer)   // 성공
